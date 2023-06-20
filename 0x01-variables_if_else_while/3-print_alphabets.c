31569f6c2a9bfaadbd3da0c09482f06198bd78ca
Sdaddy <ezemacfjsan@gmail.com>
2023-06-19 22:10:11 -0700

    alphabets in both cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints alphabets in both cases
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
